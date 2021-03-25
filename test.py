8aad9e77f25a1fb367b5d58c302bbb67aae6dc6c
wereks <mateusz.zdon@student.uj.edu.pl>
2021-03-25 01:52:12 +0100

    readme updates, pipfile cleaning, test.py refactoring

--- test.py
+++ test.py
@@ -1,41 +1,41 @@
-from utils import *
-from pathlib import Path
+import argparse
 
-import toml
+import core
 
-def setup(dir : Path, code, tags):
-    expected(dir, create=True)
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    transcript = commands.add_parser('transcript', aliases=['t'],
+                                        help = 'Tworzy tranksrypcje 16kHz plików monoaudio o rozszerzeniu .wav')
+    transcript.set_defaults(func=core.transcript)
+    transcript.add_argument('--name', '-n', help='Uruchomi funkcje tylko w podanym folderze')
+
+    #transcript.add_argument('--reset', '-r', action='store_true', help='Nadpisuje poprzednie transkrypcje')
+    #transcript.add_argument('--force', '-f', action='store_true', help='Tworzy transkrypcje nawet dla nie spelniajacych wymagan plikow .wav')
+    #transcript.add_argument('--nowarning',  '-nw', action='store_true', help='Nie wyswietli warningow')
+
+    analyze = commands.add_parser('analyze', aliases=['a'],
+                                        help = 'Tworzy wyniki (jeżeli nie istnieja) dla każdej grupy plików')
+    analyze.set_defaults(func=core.analyze)
     
+    #analyze.add_argument('--name', '-n', help='Uruchomi funkcje tylko w podanym folderze')
+    #analyze.add_argument('--reset', '-r', action='store_true', help='Nadpisuje poprzednie wyniki')
+    #analyze.add_argument('--nowarning',  '-nw', action='store_true', help='Nie wyswietli warningow')
 
-    #Stworz expected
-    #Stworz pliki db
-    #Stworz tagi
+
+    repair = commands.add_parser('repair', aliases=['r'],
+                                        help = 'Przygotowuje folder pod kolejny test case')
+    repair.add_argument('--name', '-n', required=True, help='Nazwa nowego folderu')       
+    repair.add_argument('--tag', '-t', type=str, help='Podaj liczbe, każda cyfra oznacza numer taga z configu, (dla booli 0|1 = default|~default)')
+    
+    repair.set_defaults(func=core.repair)
+
+    return parser.parse_args()
 
 
-def get_audio_files(loc : Path):
-    audio_path = loc.joinpath('audio')
-    audio_files = [x for x in audio_path.glob('*.wav') if x.is_file()]
-
-    return audio_files
-
-def expected(loc : Path, create=False):
-    expected_path = loc.joinpath('expected.txt')
-    expected_path.touch(exist_ok=True)
-
-    data = load_expected_values(expected_path)
-    audio_files = get_audio_files(loc)
-    data = { x:'-' for x in audio_files } | data
-
-    save_expected_values(expected_path, data)
-
-def tags(loc : Path, code : str):
-    #TODO
-    tags_path = loc.joinpath('tags.txt')
-    tags_path.touch(exist_ok=True)
-
-    data = load_tags_values(expected_path)
-    audio_files = get_audio_files(loc)
-    data = { x:'-' for x in audio_files } | data
-
-    save_expected_values(expected_path, data)
-
+if __name__ == '__main__':
+    args = parse_args()
+    args.func(args)
