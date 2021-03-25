179a4b4c533b155d0b9f38752a13b4c0daed62a1
wereks <mateusz.zdon@student.uj.edu.pl>
2021-03-25 02:34:32 +0100

    Model will be loaded if transcript was used at least once

--- ds.py
+++ ds.py
@@ -2,11 +2,18 @@ import wave
 import numpy as np
 import deepspeech
 
-MODEL = deepspeech.Model("deepspeech-0.9.2-models.pbmm")
-MODEL.enableExternalScorer("deepspeech-0.9.2-models.scorer")
+MODEL_NAME = "deepspeech-0.9.2-models.pbmm"
+SCORER_NAME = "deepspeech-0.9.2-models.scorer"
+MODEL = None
 
 
 def transcript(path):
+    global MODEL
+    if not MODEL:
+        MODEL = deepspeech.Model(MODEL_NAME)
+        MODEL.enableExternalScorer(SCORER_NAME)
+
+
     with wave.open(str(path), 'rb') as fin:
         audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
 
